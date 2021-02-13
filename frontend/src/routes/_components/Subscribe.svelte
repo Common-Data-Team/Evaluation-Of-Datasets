b978514b060c81d17e0c49d0bdfb75b6c4d51549
Anastasia Gisina <anastasia.gisina@inbox.ru>
2021-02-13 19:06:44 +0300

    add subscribe file

---
+++ frontend/src/routes/_components/Subscribe.svelte
@@ -0,0 +1,23 @@
+<input type="text" placeholder="Email address" name="mail" class="input-text" required>
+<input type="submit" value="Subscribe" class="input-subscribe">
+
+<style>
+
+    .input-text {
+        position: absolute;
+        width: 350px;
+        height: 50px;
+        background: #FEFEFE;
+        box-shadow: 0px 8px 80px rgba(0, 0, 0, 0.06);
+        border-color: transparent;
+        border-radius: 57px;
+    }
+
+    .input-subscribe {
+        position: absolute;
+        width: 100px;
+        height: 40px;
+        background-color: #1355FF;
+        border-color: transparent;
+    }
+</style>
