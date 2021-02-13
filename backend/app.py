a0105faa9a856e76f75e827283e853ba02a68ba5
Dmitriy Din <dmitriy1d01@gmail.com>
2021-02-13 19:39:50 +0300

    Added test file

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,24 @@
+import re
 import uvicorn
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Body
 import pandas as pd
 from io import BytesIO
 from column_analyzer import get_chart_data
 
 
 app = FastAPI()
+email_template = re.compile('^[^@]+@[^@.]+\.[^@]+$')
 
 
 @app.post('/upload/')
 def process(file: UploadFile = File(...)):
     df = pd.read_csv(BytesIO(file.file.read()))
     return {'charts': get_chart_data(df)}
+
+
+@app.post('/email/{email}')
+def save_email(email: str):
+    email_template.findall(email)
 
 
 if __name__ == '__main__':
