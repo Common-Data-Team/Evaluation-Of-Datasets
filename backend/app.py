8f0325da221f481f67163bfbac1483796022381a
Dmitriy Din <dmitriy1d01@gmail.com>
2021-02-13 19:27:36 +0300

    inited

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,17 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import pandas as pd
+from io import BytesIO
+from column_analyzer import get_chart_data
+
 
 app = FastAPI()
 
 
-@app.post('/upload')
-def process():
-    pass
+@app.post('/upload/')
+def process(file: UploadFile = File(...)):
+    df = pd.read_csv(BytesIO(file.file.read()))
+    return {'charts': get_chart_data(df)}
 
 
 if __name__ == '__main__':
